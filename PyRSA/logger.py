a32e1dc6ffa29129d8e33cede2c4750cd9c42153
Tommi Leiritie <topselius@gmail.com>
2021-09-13 21:40:13 +0300

    ignore unused modules

--- PyRSA/logger.py
+++ PyRSA/logger.py
@@ -1,4 +1,4 @@
-from logging import basicConfig, info, warning, error, DEBUG
+from logging import basicConfig, info, warning, error, DEBUG  # noqa: F401
 
 # TODO use existing log format
 basicConfig(format='%(asctime)s: %(funcName)s %(levelname)s %(message)s', level=DEBUG)
